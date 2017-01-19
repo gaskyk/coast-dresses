8aa2a273101bf18a21f1875f62cec882f49cd090
Karen Gask <onsbigdata@gmail.com>
2017-01-19 17:13:55 +0000

    First commit

---
+++ Analyse Coast dresses.R
@@ -0,0 +1,27 @@
+##############################################
+###                                        ###
+###  Analyse data about Coast dresses      ###
+###  from web scraped data                 ###
+###                                        ###
+##############################################
+
+## Import libraries
+library(ggplot2)
+
+## Import data and look at it
+data <- read.csv("C:/Users/ONS-BIG-DATA/Documents/Web scraping/Coast_dresses_all.csv",
+                 header = TRUE, sep = ",")
+str(data)
+
+## Which size has most stock?
+
+
+
+
+
+
+
+
+
+
+
